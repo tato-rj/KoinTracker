4f9ff2456cf7a2a3fb686c217eb6132157e20155
Arthur Villar <=>
2021-04-27 11:33:52 -0400

    Add badges label

--- resources/views/components/badges/all.blade.php
+++ resources/views/components/badges/all.blade.php
@@ -1,4 +1,5 @@
 @php($points = 0)
+<p class="text-grey text-muted">Earn badges as you add transactions and achieve milestones in your investments!</p>
 <div class="d-flex justify-content-center mb-4 flex-wrap" id="user-badges">
 	@foreach($badges as $badge)
 		@include('components.badges.badge')
