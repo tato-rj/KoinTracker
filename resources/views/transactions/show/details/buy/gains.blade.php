d0698b6283d9ba8652936df85d88fb8919a7389b
Arthur Villar <=>
2021-04-27 11:36:19 -0400

    Add badges label

--- resources/views/transactions/show/details/buy/gains.blade.php
+++ resources/views/transactions/show/details/buy/gains.blade.php
@@ -1,4 +1,4 @@
-<div class="col-lg-3 col-md-3 col-6 col-3 p-3">
+<div class="col-lg-3 col-md-3 col-6 col-3 p-2">
 	<div class="border-right border-sm-none">
 		@label(['text' => $transaction->isPositive ? 'You gained' : 'You lost', 'icon' => $transaction->isPositive ? 'glass-cheers' : 'heart-broken', 'bold' => false])
 		<h4 class="m-0 text-{{$transaction->isPositive ? 'green' : 'red'}}">{{$transaction->isPositive ? '+' : null}}{{$transaction->difference}}</h4>
--- resources/views/transactions/show/details/buy/percentage.blade.php
+++ resources/views/transactions/show/details/buy/percentage.blade.php
@@ -1,4 +1,4 @@
-<div class="col-lg-3 col-md-3 col-6 p-3">
+<div class="col-lg-3 col-md-3 col-6 p-2">
 	<div class=""> 
 		@label(['text' => 'Percentage', 'icon' => 'chart-area', 'bold' => false])
 		<h4 class="m-0 text-{{$transaction->isPositive ? 'green' : 'red'}}">@caret(['state' => $transaction->isPositive]){{$transaction->gains()}}</h4>
