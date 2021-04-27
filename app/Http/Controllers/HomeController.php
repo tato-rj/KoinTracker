9c33b8c5a1c8bbf4caafa238ced13e88eba34c20
Arthur Villar <=>
2021-04-27 12:31:48 -0400

    Install Mailgun

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,9 +9,6 @@ class HomeController extends Controller
 {
     public function index()
     {
-\Mail::raw('Foo bar', function ($message) {
-    $message->to('arthurvillar@gmail.com');
-});
     	$coins = Coin::all();
     	$exchanges = Exchange::valid()->top(8)->get();
 
