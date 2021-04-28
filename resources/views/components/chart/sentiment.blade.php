1bb4bae6d55223c4610edc9218f6f82a0d3c13b1
Arthur Villar <=>
2021-04-28 14:34:53 -0400

    Add sentiment to graphs

--- resources/views/components/chart/sentiment.blade.php
+++ resources/views/components/chart/sentiment.blade.php
@@ -4,18 +4,15 @@
 <div class="text-right graph-sentiment">
 	{{-- {{$priceChange}} --}}
 	@if($priceChange < 0.94)
-	<div class="text-red font-weight-bold">Oh boy... @fa(['icon' => 'sad-cry', 'mr' => 0, 'size' => 'lg'])</div>
+	<div class="text-red font-weight-bold">Oh boy... @fa(['icon' => 'angry', 'mr' => 0, 'size' => 'lg'])</div>
 	@elseif(between($priceChange, 0.94, 0.96))
 	<div class="text-red font-weight-bold">Not so good... @fa(['icon' => 'flushed', 'mr' => 0, 'size' => 'lg'])</div>
 	@elseif(between($priceChange, 0.96, 0.98))
 	<div class="text-red font-weight-bold">Come on... @fa(['icon' => 'grimface', 'mr' => 0, 'size' => 'lg'])</div>
-	@elseif(between($priceChange, 0.97, 0.99))
+	@elseif(between($priceChange, 0.97, 1))
 	<div class="text-red font-weight-bold">Ugh... @fa(['icon' => 'angry', 'mr' => 0, 'size' => 'lg'])</div>
 
-	@elseif(between($priceChange, 0.99, 1.2))
-	<div class="text-warning font-weight-bold">Keep watching @fa(['icon' => 'meh', 'mr' => 0, 'size' => 'lg'])</div>
-
-	@elseif(between($priceChange, 1.2, 1.4))
+	@elseif(between($priceChange, 1, 1.4))
 	<div class="text-green font-weight-bold">Lookin' good! @fa(['icon' => 'smile', 'mr' => 0, 'size' => 'lg'])</div>
 	@elseif(between($priceChange, 1.4, 1.8))
 	<div class="text-green font-weight-bold">This is great @fa(['icon' => 'grin', 'mr' => 0, 'size' => 'lg'])</div>
