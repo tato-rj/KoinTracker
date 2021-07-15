70702986a86d076058117e698ee92580380e193c
Arthur Villar <arthur@iMac.local>
2021-07-14 22:37:10 -0400

    Remove exclamation point

--- resources/views/auth/dev.blade.php
+++ resources/views/auth/dev.blade.php
@@ -10,7 +10,7 @@
     <div class="row flex-center h-100vh">
         <div class="col-lg-4 col-md-4 col-8 mx-auto">
             @icon(['size' => 50, 'position' => 'center', 'mb' => 2])
-            <p class="text-center"><strong>We're going live soon!</strong></p>
+            <p class="text-center"><strong>We're going live soon</strong></p>
             <p class="text-muted">If you're an admin, type your password below to get in.</p>
             <form method="POST" action="{{route('dev.admin')}}" disable-on-submit>
                 @csrf
